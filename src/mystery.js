b351a174cf4a55554328f806617a5b9bf525ce41
Dennif <dennif.pa@gmail.com>
2025-05-21 11:19:58 -0700

    done

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,8 @@
-export const mystery2 = (num) => {};
+export const mystery2 = (num) => {
+  return (num % 4 === 0 && num % 100 !== 0) || (num % 400 === 0);
+};
+
+// Years not divisible by 4 return a falsy result
+// Years divisible by 400 return a truthy result (failed)
+// Years divisible by 100 return a falsy result
+// Years that are divisible by four return a truthy result (failed)
